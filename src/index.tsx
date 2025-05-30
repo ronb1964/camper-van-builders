01b4c023de72b5fd1aedf4db5d784656f70527d7
Camper Van Builder <campervanbuilder@example.com>
2025-05-29 22:05:57 -0400

    Fix: Remove bouncing animation from hero image

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './no-bounce.css'; // Import no-bounce CSS to prevent animations
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { setupGlobalErrorHandling } from './utils/errorLogger';
